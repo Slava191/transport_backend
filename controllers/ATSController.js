8e169c1f2aa1288889e6b680997e06941f2ccecb
Slava191 <slava191web@gmail.com>
2021-02-17 18:14:23 +0300

    Order by ID DESC output in ATS Controller

--- controllers/ATSController.js
+++ controllers/ATSController.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 const ATS = require("../models/ATS");
-const HodovyeKachestva = require("../models/hodovyeKachestva");
-const Gabarity = require("../models/gabarity")
+// const HodovyeKachestva = require("../models/hodovyeKachestva");
+// const Gabarity = require("../models/gabarity")
 
 //const sequelize = require("../libs/sequelize")
 
@@ -9,8 +9,13 @@ exports.getAllATS = async function(req, res){
 
     try{
 
-        let allATS = await ATS.findAll({raw:true, include: [User, HodovyeKachestva, Gabarity]})
-        console.log(allATS)
+        let allATS = await ATS.findAll({
+            raw:true, 
+            include: [User],
+            order: [
+                ['id', 'DESC']
+            ]
+        })
 
         //let [ allATS ] = await sequelize.query('SELECT a.*, u.login FROM ATs a INNER JOIN users u ON a.user_id = u.id')
 
